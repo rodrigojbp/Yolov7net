35b045b27c1c52046ea5231d6891fb094d77049e
Rodrigo dos Passos <rodrigo.dospassos@dataprev.gov.br>
2022-12-22 01:22:24 -0300

    refactor code

--- WinFormsAppYoloV7/Form1.cs
+++ WinFormsAppYoloV7/Form1.cs
@@ -36,40 +36,26 @@ namespace WinFormsAppYoloV7
 			using var yolo = new Yolov7("Assets/yolov7-tiny.onnx", checkBoxUseGPU.Checked);
 			// setup labels of onnx model 
 			yolo.SetupYoloDefaultLabels();   // use custom trained model should use your labels like: yolo.SetupLabels(string[] labels)
-			using var image = Image.FromFile(fileName);
-			var predictions = yolo.Predict(image);
+			DrawRectangle(yolo);
 
-			// draw box
-			using var graphics = Graphics.FromImage(image);
-			foreach (var prediction in predictions) // iterate predictions to draw results
-			{
-				double score = Math.Round(prediction.Score, 2);
-				graphics.DrawRectangles(new Pen(prediction.Label.Color, 1), new[] { prediction.Rectangle });
-				var (x, y) = (prediction.Rectangle.X - 3, prediction.Rectangle.Y - 23);
-				graphics.DrawString($"{prediction.Label.Name} ({score})",
-								new Font("Consolas", 32, GraphicsUnit.Pixel), new SolidBrush(prediction.Label.Color),
-								new PointF(x, y));
-			}
-
-			pictureBox1.Image = image;
-			pictureBox1.Refresh();
-
-			MessageBox.Show("Predição finalizada.");
+			MessageBox.Show("Finished v7.");
 		}
 
 		private void button3_Click(object sender, EventArgs e)
 		{
 			// init Yolov7 with onnx (include nms results)file path
-
-			//using var yolo = new Yolov5(@"E:\dev\yolo\dotnet\Yolov7net\WinFormsAppYoloV7\Assets\yolov7-tiny_640x640.onnx", false);
 			using var yolo = new Yolov7("Assets/yolov7-tiny_640x640.onnx", checkBoxUseGPU.Checked);
 			// setup labels of onnx model 
 			yolo.SetupYoloDefaultLabels();   // use custom trained model should use your labels like: yolo.SetupLabels(string[] labels)
-			using var image = Image.FromFile(fileName);
-			var predictions = yolo.Predict(image);
+			DrawRectangle(yolo);
+			MessageBox.Show("Finish v5.");
+		}
 
-			// draw box
-			using var graphics = Graphics.FromImage(image);
+		private void DrawRectangle(Yolov7 yolo)
+		{
+			Image image = Image.FromFile(fileName);
+			var predictions = yolo.Predict(image);
+			Graphics graphics = Graphics.FromImage(image);
 			foreach (var prediction in predictions) // iterate predictions to draw results
 			{
 				double score = Math.Round(prediction.Score, 2);
@@ -81,10 +67,6 @@ namespace WinFormsAppYoloV7
 			}
 			pictureBox1.Image = image;
 			pictureBox1.Refresh();
-			
-
-			MessageBox.Show("Predição finalizada.");
 		}
-
 	}
 }
