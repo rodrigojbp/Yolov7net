0375f65d4465a36923da4e70b7bb322ce8e05aa2
Rodrigo dos Passos <rodrigo.dospassos@dataprev.gov.br>
2022-12-22 23:35:24 -0300

    Adicionando suporte a webcam

---
+++ src/Yolov7net/Models/Abstract/YoloModelAbstract.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using Yolov7net;
+
+namespace Yolov7net.Models.Abstract
+{
+    /// <summary>
+    /// Model descriptor.
+    /// </summary>
+    public abstract class YoloModelAbstract
+    {
+        public abstract int Width { get; }
+        public abstract int Height { get; }
+        public abstract int Depth { get; }
+
+        public abstract int Dimensions { get; }
+
+        public abstract float[] Strides { get; }
+        public abstract float[][][] Anchors { get; }
+        public abstract int[] Shapes { get; }
+
+        public abstract float Confidence { get; }
+        public abstract float MulConfidence { get; }
+        public abstract float Overlap { get; }
+
+        public abstract string[] OutputNames { get; }
+
+        public abstract bool UseDetect { get; }
+
+        public abstract string Weights { get; }
+
+        public List<YoloLabel> Labels { get; protected set; }
+
+        public bool Ready { get; set; }
+    }
+}
